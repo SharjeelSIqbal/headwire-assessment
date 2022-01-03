129b958024676fe51c1cc355943526587ea6c4d9
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2022-01-03 08:01:37 -0800

    Put a transition delay on the head link so it won't disappear right away

--- src/components/header-section/header-drop-down.jsx
+++ src/components/header-section/header-drop-down.jsx
@@ -1,10 +1,9 @@
 import HeaderDropDownList from "./header-drop-down-list"
 
-const HeaderDropDown = ({ isActive, dropDownList }) => {
+const HeaderDropDown = ({ isActive, dropDownList, heading }) => {
   const className = isActive && dropDownList.length > 0
-    ? 'header-drop-down'
+    ? `header-${heading}-drop-down`
     : 'hidden'
-
   return (
     <div className={className}>
       <HeaderDropDownList list={dropDownList}  />
