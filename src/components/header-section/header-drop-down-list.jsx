f8cbe6cd198fb5c0054610591532ed8812a46066
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2022-01-03 02:50:28 -0800

    finished the drop down menu for header with animation

--- src/components/header-section/header-drop-down-list.jsx
+++ src/components/header-section/header-drop-down-list.jsx
@@ -1,11 +1,17 @@
-const HeaderDropDownList = ({ list  }) => (
-  <ul className='header-drop-down-list'>
-    {
-      list.map(listItem => (
-        <li className='header-drop-down-list-item'>{listItem}</li>
-      ))
-    }
-  </ul>
-)
+const HeaderDropDownList = ({ list }) => {
+  return (
+    <ul className='header-drop-down-list titillium white-text text-transform-normal p-1'>
+      {
+        list.map(listItem => (
+          <li className='header-drop-down-list-item p-pt5 flex align-center'>
+            <div className='header-drop-down-border' />
+            {listItem}
+          </li>
+        ))
+      }
+    </ul>
+    )
+  }
+
 
 export default HeaderDropDownList
--- src/components/header-section/header-drop-down.jsx
+++ src/components/header-section/header-drop-down.jsx
@@ -1,11 +1,18 @@
 import HeaderDropDownList from "./header-drop-down-list"
 
-const HOME_DROP_DOWN_LIST = [ 'Main Home', 'Interior Design', 'Studio Home', 'Architecture Agency', 'Design Company', 'Home Video', 'Home Fullscreen', 'Home One Page', 'Home with Slide Menu', ]
 
-const HeaderDropDown = () => (
-  <div className='header-drop-down'>
-    <HeaderDropDownList list={HOME_DROP_DOWN_LIST}  />
-  </div>
-)
+
+const HeaderDropDown = ({ isActive, dropDownList }) => {
+  const className = isActive && dropDownList.length > 0
+    ? 'header-drop-down'
+    : 'hidden'
+  console.log(isActive)
+
+  return (
+    <div className={className}>
+      <HeaderDropDownList list={dropDownList}  />
+    </div>
+  )
+}
 
 export default HeaderDropDown
