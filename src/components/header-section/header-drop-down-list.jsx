80c3d706039e8d764e8e17702acdc0f370c4d419
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2022-01-03 02:53:58 -0800

    fixed some styling issues removed console logs and refactored code

--- src/components/header-section/header-drop-down-list.jsx
+++ src/components/header-section/header-drop-down-list.jsx
@@ -1,17 +1,14 @@
-const HeaderDropDownList = ({ list }) => {
-  return (
-    <ul className='header-drop-down-list titillium white-text text-transform-normal p-1'>
-      {
-        list.map(listItem => (
-          <li className='header-drop-down-list-item p-pt5 flex align-center'>
-            <div className='header-drop-down-border' />
-            {listItem}
-          </li>
-        ))
-      }
-    </ul>
-    )
-  }
-
+const HeaderDropDownList = ({ list }) => (
+  <ul className='header-drop-down-list titillium white-text text-transform-normal p-1'>
+    {
+      list.map(listItem => (
+        <li className='header-drop-down-list-item p-pt5 flex align-center'>
+          <div className='header-drop-down-border' />
+          {listItem}
+        </li>
+      ))
+    }
+  </ul>
+  )
 
 export default HeaderDropDownList
