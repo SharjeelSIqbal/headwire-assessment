4511f93d818826c2c78660d15606f18cf906ebfa
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2022-01-03 00:06:39 -0800

    Made carousel for the background image for the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,50 @@
-import { HeaderSidebar, HeaderLogo, HeaderHead, HeaderMenu, HeaderCenter } from './header-section'
+import {  HeaderSidebar, HeaderLogo, HeaderHead, HeaderMenu, HeaderCenter } from './header-section'
 import { VscArrowRight, VscArrowLeft } from 'react-icons/vsc'
 import '../css/header.css'
+import { useState, useEffect } from 'react'
 
-const HeaderSection = () => {
-  const background = [{
-    img:'/imgs/slider1-home1.jpeg',
-    text: 'design'
-  }, {
-    img: '/imgs/slider2-home1.jpeg',
-    text: 'art'
-  }, {
-    img:'/imgs/slider3-home1.jpeg',
-    text: 'studio'
-  }]
+const SLIDE_COUNTDOWN = 4000
+const SLIDE = [{
+  img: '/imgs/slider1-home1.jpeg',
+  text: 'design',
+  heading: 'Panel of Interior',
+}, {
+  img: '/imgs/slider2-home1.jpeg',
+  text: 'art',
+  heading: 'Masterful Art'
+}, {
+  img: '/imgs/slider3-home1.jpeg',
+  text: 'studio',
+  heading: 'Best Furniture and Decor'
+}]
+
+const Header = () => {
+  const [ currentSlideIndex, setCurrentSlideIndex ] = useState(0)
+
+  const currentSlide = SLIDE[currentSlideIndex]
+
+  const nextSlide = () => {
+    if (currentSlideIndex !== SLIDE.length - 1) {
+      setCurrentSlideIndex(currentSlideIndex + 1)
+    } else {
+      setCurrentSlideIndex(0)
+    }
+  }
+
+  const prevSlide = () => {
+    if (currentSlideIndex !== 0) {
+      setCurrentSlideIndex(currentSlideIndex - 1)
+    } else {
+      setCurrentSlideIndex(SLIDE.length - 1)
+    }
+  }
+
+  useEffect(() => {
+    let slideChangeTimeout = setTimeout(nextSlide, SLIDE_COUNTDOWN)
+
+    return () => clearTimeout(slideChangeTimeout)
+    // eslint-disable-next-line
+  }, [ currentSlideIndex ])
 
   return (
     <div id='header' className='header-grid relative'>
@@ -20,21 +52,21 @@ const HeaderSection = () => {
       <HeaderHead />
       <HeaderMenu />
       <HeaderSidebar />
-      <HeaderCenter background={background[2]} />
+      <HeaderCenter slide={currentSlide} />
       <div className='header-nothing' />
       <div className='header-bottom' />
       <div className='header-next'>
-        <button className='header-carousel-button header-next-button'>
+        <button onClick={nextSlide} className='header-carousel-button header-next-button'>
           <VscArrowRight />
         </button>
       </div>
       <div className='header-prev flex justify-end'>
-        <button className='header-carousel-button p-0'>
-          <VscArrowLeft/>
+        <button onClick={prevSlide} className='header-carousel-button p-0'>
+          <VscArrowLeft />
         </button>
       </div>
     </div>
   )
 }
 
-export default HeaderSection
+export default Header
